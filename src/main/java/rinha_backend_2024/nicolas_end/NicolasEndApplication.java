563294eb9383086bc4f8b559c052f6ca24e796d2
Nicolas-End <nicolasvalsi203.com>
2026-01-25 16:16:27 -0300

    [feat] adding spring boot packages

---
+++ src/main/java/rinha_backend_2024/nicolas_end/NicolasEndApplication.java
@@ -0,0 +1,13 @@
+package rinha_backend_2024.nicolas_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NicolasEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NicolasEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rinha_backend_2024/nicolas_end/NicolasEndApplicationTests.java
@@ -0,0 +1,13 @@
+package rinha_backend_2024.nicolas_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NicolasEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
